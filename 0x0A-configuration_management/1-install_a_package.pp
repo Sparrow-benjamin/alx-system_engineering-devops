fbdfe697ebe89982df7244f34ef3e40a5ec49c61
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2023-03-25 04:03:27 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Manifest that installs puppet-lint version 2.5.0
-package { 'puppet-lint':
+# Manifest that installs puppet-lint version 2.1.0
+package { 'puppet-flask':
   ensure   => '2.1.0',
-  provider => 'gem'
+  provider => 'pip3'
 }
