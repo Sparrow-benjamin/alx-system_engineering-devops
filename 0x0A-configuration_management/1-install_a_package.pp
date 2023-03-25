98cee7d8e27ef2f123f54d63b8c7ddeb1469eb57
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2023-03-25 04:01:06 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-// install puppet lint
+# Manifest that installs puppet-lint version 2.5.0
 package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+  ensure   => '2.1.0',
+  provider => 'gem'
 }
